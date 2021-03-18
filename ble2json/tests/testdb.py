5d5fa18ff0977fff54159e5cee0c88b07a7aafef
teuvo486 <53901404+teuvo486@users.noreply.github.com>
2021-03-18 14:04:11 +0200

    Make the testdb script faster

--- ble2json/tests/testdb.py
+++ ble2json/tests/testdb.py
@@ -1,70 +1,50 @@
 import struct
 import sqlite3
 from os.path import relpath
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from ble2json import db
-from ble2json.device import sensordata, ruuvi5
+from ble2json.device import ruuvi5
 
-DELTA = timedelta(seconds=20)
-RATE_LIMIT = timedelta(minutes=5).total_seconds()
-TEST_DEVS = [
-    {
-        "name": "example1",
-        "address": "00:00:00:00:00:01",
-        "objPath": "/org/bluez/hci0/dev_00_00_00_00_00_01",
-        "format": "ruuvi5",
-    },
-    {
-        "name": "example2",
-        "address": "00:00:00:00:00:02",
-        "objPath": "/org/bluez/hci0/dev_00_00_00_00_00_02",
-        "format": "ruuvi5",
-    },
-    {
-        "name": "example3",
-        "address": "00:00:00:00:00:03",
-        "objPath": "/org/bluez/hci0/dev_00_00_00_00_00_03",
-        "format": "ruuvi5",
-    },
-]
-
-
-def generate(start, end):
-    start = datetime.fromisoformat(start)
-    end = datetime.fromisoformat(end)
+NAME = "example1"
+ADDR = "00:00:00:00:00:01"
+OBJ_PATH = "/org/bluez/hci0/dev_00_00_00_00_00_01"
+FMT = "ruuvi5"
+START = int((datetime.now() - timedelta(days=365)).timestamp())
+END = int(datetime.now().timestamp())
+DELTA = int(timedelta(minutes=5).total_seconds())
+    
+def generate():
     db_path = "/dev/shm/ble2json.db"
     conn = db.connect(db_path)
 
     with open(relpath("ble2json/schema.sql")) as f:
         conn.executescript(f.read())
 
-    for dev in TEST_DEVS:
-        name = dev["name"]
-        addr = dev["address"]
-        obj_path = dev["objPath"]
-        fmt = dev["format"]
+    conn.execute(
+        """INSERT INTO device (name, address, objPath, format)
+           VALUES (?, ?, ?, ?)
+           ON CONFLICT DO NOTHING""",
+        (NAME, ADDR, OBJ_PATH, FMT),
+    )
 
-        conn.execute(
-            """INSERT INTO device (name, address, objPath, format)
-               VALUES (?, ?, ?, ?)
-               ON CONFLICT DO NOTHING""",
-            (name, addr, obj_path, fmt),
-        )
-
-        conn.commit()
-        current = start
-
-        while current < end:
-            current += DELTA
-            time = int(current.timestamp())
-            mfdata = generate_test_data(time)
-            sensordata.insert(db_path, obj_path, RATE_LIMIT, time, mfdata)
+    conn.commit()
+        
+    conn.executemany(
+        """INSERT INTO data
+           VALUES ((SELECT id FROM device WHERE objPath = ?), 
+           ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", generator()
+    )
 
     conn.commit()
     conn.close()
 
 
-def generate_test_data(time):
+def generator():
+    for t in range(START, END, DELTA):
+        yield test_data(t)
+
+
+def test_data(time):
     b = struct.pack(
         ruuvi5.FMT,
         5,
@@ -85,4 +65,20 @@ def generate_test_data(time):
         0,
     )
 
-    return {0x0499: b}
+    data = ruuvi5.from_bytes(b)    
+
+    return (
+        OBJ_PATH,
+        time,
+        data.get("temperature"),
+        data.get("humidity"),
+        data.get("pressure"),
+        data.get("accelerationX"),
+        data.get("accelerationY"),
+        data.get("accelerationZ"),
+        data.get("voltage"),
+        data.get("txPower"),
+        data.get("movementCounter"),
+        data.get("measurementSequence"),
+    )
+    
