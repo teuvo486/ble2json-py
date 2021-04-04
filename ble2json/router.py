b0eef1111c6508dc3ea4ca2e6ca7f76c03de9cde
teuvo486 <53901404+teuvo486@users.noreply.github.com>
2021-04-04 20:17:44 +0300

    Leave compression to the frontend server

--- ble2json/router.py
+++ ble2json/router.py
@@ -1,5 +1,4 @@
-import gzip
-from flask import abort, Blueprint, json, jsonify, request, make_response
+from flask import abort, Blueprint, jsonify, request
 from . import device
 
 bp = Blueprint("router", __name__)
@@ -11,10 +10,6 @@ def get_all():
     end = request.args.get("end")
     cols = request.args.get("columns")
     devs = device.get_all(start, end, cols)
-
-    if "gzip" in request.accept_encodings:
-        return compress(devs)
-
     return jsonify(devs)
 
 
@@ -28,18 +23,7 @@ def get_one(name):
     if not dev:
         abort(404)
 
-    if "gzip" in request.accept_encodings:
-        return compress(dev)
-
     return dev
-
-
-def compress(data):
-    b = bytes(json.dumps(data), "UTF-8")
-    res = make_response(gzip.compress(b))
-    res.headers["Content-Type"] = "application/json"
-    res.headers["Content-Encoding"] = "gzip"
-    return res
 
 
 def cors(res):
